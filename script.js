e8789ae6b4d3f7523a60821427bc3a6a73b23cbc
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2016-07-03 14:20:41 -0700

    Problems with either undefined or duplicately added ModuleA

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-var moduleA = angular.module("MyModuleA", []);
-moduleA.controller("MyControllerA", function($scope) {
-    $scope.name = "Bob A";
-});
 
-var moduleB = angular.module("MyModuleB", []);
-moduleB.controller("MyControllerB", function($scope) {
-    $scope.name = "Steve B";
-});
+
+(function () {
+    "use strict";
+
+    var moduleRt,
+        moduleA,
+        moduleB;
+
+    moduleA = angular.module("MyModuleA", []);
+    moduleA.controller("MyControllerA", function ($scope) {
+        $scope.name = "Bob A";
+    });
+
+    moduleB = angular.module("MyModuleB", []);
+    moduleB.controller("MyControllerB", function ($scope) {
+        $scope.name = "Steve B";
+    });
+
+    moduleRt = angular.module("RtMod", ["MyModuleA", "MyModuleB"]);
+    moduleRt.controller("RtCtrl", function ($scope) {
+        $scope.name = "Root";
+    });
+
+}());
