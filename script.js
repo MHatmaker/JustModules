3efdeb5f862798f1fda59f0c63b089542ade9c55
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2016-07-04 12:20:32 -0700

    Works with emit/broadcast

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Code goes here
-
-
 
 (function () {
     "use strict";
@@ -12,16 +9,30 @@
     moduleA = angular.module("MyModuleA", []);
     moduleA.controller("MyControllerA", function ($scope) {
         $scope.name = "Bob A";
+
+        $scope.$on("BroadcastEvent", function(evt, args) {
+            console.log("BroadcastEvent in A");
+        });
     });
 
     moduleB = angular.module("MyModuleB", []);
     moduleB.controller("MyControllerB", function ($scope) {
         $scope.name = "Steve B";
+
+        $scope.clickB = function() {
+            console.log("ClickB");
+            $scope.$emit("BClickerEvent");
+        }
     });
 
     moduleRt = angular.module("RtMod", []);
     moduleRt.controller("RtCtrl", function ($scope) {
         $scope.name = "Root";
+
+        $scope.$on("BClickerEvent", function(evt, args) {
+            console.log("BClickerEvent caught in Root");
+            $scope.$broadcast("BroadcastEvent");
+        });
     });
 
 }());
